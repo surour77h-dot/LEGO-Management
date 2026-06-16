0995f8c6853037867e3a64b015e2d212253fcc0d
Hamad77S <surour77h@gmail.com>
2026-06-16 22:41:01 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='hskw-lego-v4-20260612';
+const CACHE='hskw-lego-v5-20260613';
 self.addEventListener('install',e=>{self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.map(c=>caches.delete(c)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request,{cache:'reload'}).catch(()=>caches.match(e.request)));});
